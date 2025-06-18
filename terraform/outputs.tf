9c8c8620ac7ae2df728fa60fbdfd01e5bea45875
karthik Duggana <d.karthikyadav94@gmail.com>
2025-06-18 15:33:47 +0530

    ecs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,13 @@
+# data "aws_ecs_task" "example" {
+#   cluster = aws_ecs_cluster.my_cluster.id
+#   task_id = "<TASK_ID>"  # You need to know this or fetch dynamically using `external` data source or CLI.
+# }
 
-output "ecs_cluster_name" {
-  description = "The name of the ECS cluster."
-  value       = aws_ecs_cluster.main.name
-}
+# data "aws_network_interface" "task_eni" {
+#   id = data.aws_ecs_task.example.attachments[0].details[?key=="networkInterfaceId"][0].value
+# }
 
-output "ecs_service_name" {
-  description = "The name of the ECS service."
-  value       = aws_ecs_service.main.name
-}
+# output "ecs_fargate_task_public_ip" {
+#   description = "Public IP of the ECS Fargate task"
+#   value       = data.aws_network_interface.task_eni.association[0].public_ip
+# }
