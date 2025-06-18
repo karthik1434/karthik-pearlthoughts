29389c60aa5b8d410bdc5037cc6f25398bf126f5
karthik Duggana <d.karthikyadav94@gmail.com>
2025-06-18 15:19:41 +0530

    ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -16,12 +16,6 @@ resource "aws_ecs_service" "main" {
     assign_public_ip = true
   }
 
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.main.arn
-#     container_name   = "${var.name}-strapi"
-#     container_port   = 1337
-#   }
-  # This ensures that a new deployment is triggered when the task definition changes
   force_new_deployment = true
 }
 
@@ -32,8 +26,7 @@ resource "aws_ecs_task_definition" "app" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-#   execution_role_arn       = "arn:aws:iam::123456789012:role/YourExistingEcsRoleName"
+  execution_role_arn       = "arn:aws:iam::607700977843:role/your-ExistingEcsRoleName"
 
   container_definitions = jsonencode([
     {
@@ -88,29 +81,4 @@ resource "aws_security_group" "task_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-
-
-
-# IAM Role for ECS Task Execution
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "${var.name}-ecs-task-execution-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,29 +1,3 @@
-# output "load_balancer_dns" {
-#   description = "The DNS name of the Application Load Balancer."
-#   value       = aws_lb.main.dns_name
-# }
-
-
-# This data source is needed to fetch the public IP after the service is stable
-output "instructions" {
-  description = "Instructions to find the public IP of your container."
-  value = <<EOT
-Your Strapi container has been deployed. Because the Public IP is assigned dynamically by AWS,
-you can find it after deployment using one of the methods below.
-
-1. AWS Management Console:
-   - Navigate to the ECS service named '${aws_ecs_service.main.name}' in the cluster '${aws_ecs_cluster.main.name}'.
-   - Click on the 'Tasks' tab.
-   - Click on the running task ID.
-   - In the 'Network' section, you will find the 'Public IP'.
-
-2. AWS CLI:
-   - You can get the IP using the following command (requires AWS CLI and jq):
-   aws ecs describe-tasks --cluster ${aws_ecs_cluster.main.name} --tasks $(aws ecs list-tasks --cluster ${aws_ecs_cluster.main.name} --service-name ${aws_ecs_service.main.name} --query 'taskArns[0]' --output text) --query 'tasks[0].attachments[0].details[?name==`networkInterfaceId`].value | [0]' --output text | xargs -I {} aws ec2 describe-network-interfaces --network-interface-ids {} --query 'NetworkInterfaces[0].Association.PublicIp' --output text
-
-Your application will be available at: http://<YOUR_PUBLIC_IP>:1337
-EOT
-}
 
 output "ecs_cluster_name" {
   description = "The name of the ECS cluster."
