082b636c01381dadce01a75198400ff8039a4cb0
karthik Duggana <d.karthikyadav94@gmail.com>
2025-06-19 12:18:43 +0530

    cloud watch

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -19,6 +19,20 @@ resource "aws_ecs_service" "main" {
   force_new_deployment = true
 }
 
+
+data "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_policy" {
+  role       = data.aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+
+
+
 # ECS Task Definition
 resource "aws_ecs_task_definition" "app" {
   family                   = "${var.name}-task"
@@ -26,7 +40,8 @@ resource "aws_ecs_task_definition" "app" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = "arn:aws:iam::607700977843:role/ecsTaskExecutionRole"
+  # execution_role_arn       = "arn:aws:iam::607700977843:role/ecsTaskExecutionRole"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
     {
