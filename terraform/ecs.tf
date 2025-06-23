51b3f1d14d7d3c4a0fd1d582b137f178c1d1f63e
karthik Duggana <d.karthikyadav94@gmail.com>
2025-06-23 10:41:47 +0530

    task-10

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -69,15 +69,6 @@ resource "aws_ecs_task_definition" "app" {
           hostPort      = 1337
         }
       ]
-
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.strapi.name
-          awslogs-region        = var.region
-          awslogs-stream-prefix = "ecs/strapi"
-        }
-      }
     #   environment = [
     #     {
     #       name  = "APP_KEYS",
